d4c6c5185a91268254cc9c570a7cf1c2c7dd0eb4
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2021-10-07 17:04:28 -0600

    Deleted faulty window listener that resulted in an irritating mobile experience.
    The automatic resizing which occurs in mobile browsers caused a recurring resetting of the lesson information section.  The code was deleted, and I am seeking a new solution for a display issue which occurs when the browser window is resized small (to a mobile size), lesson info tabs are opened, and the window is made large again.  In this instance, descriptive text may be displayed overlapping other descriptive text until one of the options on the left is clicked.

--- lessons.js
+++ lessons.js
@@ -1,18 +1,6 @@
-let l = document.getElementById("lib").children;
+let l = document.getElementById("lessonInfoBox").children;
 
 function listen() {
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 700) {
-      for (let i = 1; i < l.length; i += 2) {
-        l[i].style.display = "none";
-        l[i - 1].style.borderRight = "solid 1px rgb(19, 64, 77)";
-      }
-    } else {
-      for (let x = 0; x <= 4; x += 2) {
-        l[x].style.borderRight = "none";
-      }
-    }
-  });
   for (let i = 0; i <= 5; i += 2) {
     l[i].addEventListener("click", () => {
       if (window.innerWidth > 700) {
@@ -33,3 +21,9 @@ function listen() {
 }
 
 listen();
+
+// const lib = document.getElementById("lessonInfoBox");
+
+// lib.addEventListener("click", (e) => {
+//   console.log(e.target);
+// });
