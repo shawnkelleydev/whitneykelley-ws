b0ad19461a79178201678b467c58defd112f940b
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2021-10-07 17:15:56 -0600

    Fixed problems with lesson information section.
    Removed conditional statement such that only one tab can be open at a time, which precluded other issues that were occurring in transition from mobile to desktop views.

--- lessons.js
+++ lessons.js
@@ -3,11 +3,10 @@ let l = document.getElementById("lessonInfoBox").children;
 function listen() {
   for (let i = 0; i <= 5; i += 2) {
     l[i].addEventListener("click", () => {
-      if (window.innerWidth > 700) {
-        for (let i = 1; i < l.length; i += 2) {
-          l[i].style.display = "none";
-        }
+      for (let i = 1; i < l.length; i += 2) {
+        l[i].style.display = "none";
       }
+
       if (l[i + 1].style.display === "none" || l[i + 1].style.display === "") {
         l[i + 1].style.display = "block";
         for (let x = 0; x <= 4; x += 2) {
